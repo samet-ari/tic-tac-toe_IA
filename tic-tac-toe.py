b2f0c8d8a7405da53e6c02790710e886016b5090
unknown <vanessa.sabatier@laplateforme.io>
2024-12-02 15:26:35 +0100

    bug fixed tictactoe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -65,6 +65,82 @@ def print_scoreboard(score_board):
 # main
 if __name__ == "__main__":
 
+    print("Player 1")
+    player1 = input("Enter the name : ")
+    print("\n")
+ 
+    print("Player 2")
+    player2 = input("Enter the name : ")
+    print("\n")
+     
+    # Stores the player who chooses X and O
+    cur_player = player1
+ 
+    # Stores the choice of players
+    player_choice = {'X' : "", 'O' : ""}
+ 
+    # Stores the options
+    options = ['X', 'O']
+ 
+    # Stores the scoreboard
+    score_board = {player1: 0, player2: 0}
+    print_scoreboard(score_board)
+ 
+    # Game Loop for a series of Tic Tac Toe
+    # The loop runs until the players quit 
+    while True:
+ 
+        # Player choice Menu
+        print("Turn to choose for", cur_player)
+        print("Enter 1 for X")
+        print("Enter 2 for O")
+        print("Enter 3 to Quit")
+ 
+        # Try exception for CHOICE input
+        try:
+            choice = int(input())   
+        except ValueError:
+            print("Wrong Input!!! Try Again\n")
+            continue
+ 
+        # Conditions for player choice  
+        if choice == 1:
+            player_choice['X'] = cur_player
+            if cur_player == player1:
+                player_choice['O'] = player2
+            else:
+                player_choice['O'] = player1
+ 
+        elif choice == 2:
+            player_choice['O'] = cur_player
+            if cur_player == player1:
+                player_choice['X'] = player2
+            else:
+                player_choice['X'] = player1
+         
+        elif choice == 3:
+            print("Final Scores")
+            print_scoreboard(score_board)
+            break  
+ 
+        else:
+            print("Wrong Choice!!!! Try Again\n")
+ 
+        # Stores the winner in a single game of Tic Tac Toe
+        winner = single_game(options[choice-1])
+         
+        # Edits the scoreboard according to the winner
+        if winner != 'D' :
+            player_won = player_choice[winner]
+            score_board[player_won] = score_board[player_won] + 1
+ 
+        print_scoreboard(score_board)
+        # Switch player who chooses X or O
+        if cur_player == player1:
+            cur_player = player2
+        else:
+            cur_player = player1
+
     print_tic_tac_toe(values)
 
 
